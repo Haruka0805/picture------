4867b8e72043eece99c101cf002dfca9f324265d
Haruka0805 <gc231116@senshu-u.jp>
2025-10-23 17:16:21 +0900

    works.js を作成

---
+++ works.js
@@ -0,0 +1,14 @@
+window.WORKS = [
+  { title:"ネクタイ",       date:"", yarn:"", needle:"", hours:"", tags:["小物","ネクタイ"], img:"images/tie.jpg",       alt:"ネクタイ" },
+  { title:"ナップサック",   date:"", yarn:"", needle:"", hours:"", tags:["バッグ"],         img:"images/bag.jpg",       alt:"ナップサック" },
+  { title:"猫耳帽子",       date:"", yarn:"", needle:"", hours:"", tags:["帽子"],           img:"images/cathat.jpg",    alt:"猫耳帽子" },
+  { title:"ヘッドドレス",   date:"", yarn:"", needle:"", hours:"", tags:["アクセ"],         img:"images/headdress.jpg", alt:"ヘッドドレス" },
+  { title:"キティちゃんポーチ", date:"", yarn:"", needle:"", hours:"", tags:["ポーチ"],   img:"images/kitty.jpg",     alt:"キティちゃんポーチ" },
+  { title:"ボンネット帽",   date:"", yarn:"", needle:"", hours:"", tags:["帽子"],           img:"images/knithat.jpg",   alt:"ボンネット帽" },
+  { title:"ミッフィー",     date:"", yarn:"", needle:"", hours:"", tags:["マスコット"],     img:"images/miffy.jpg",     alt:"ミッフィー" },
+  { title:"ペンライトケース", date:"", yarn:"", needle:"", hours:"", tags:["小物"],        img:"images/pencase.jpg",   alt:"ペンライトケース" },
+  { title:"ペンポーチ",     date:"", yarn:"", needle:"", hours:"", tags:["ポーチ"],         img:"images/penpouch.jpg",  alt:"ペンポーチ" },
+  { title:"てるてる坊主",   date:"", yarn:"", needle:"", hours:"", tags:["マスコット"],     img:"images/teruteru.jpg",  alt:"てるてる坊主" },
+  { title:"お花のポーチ",   date:"", yarn:"", needle:"", hours:"", tags:["ポーチ"],         img:"images/whitebag.jpg",  alt:"お花のポーチ" },
+  { title:"スヌーピーのあみぐるみ", date:"", yarn:"", needle:"", hours:"", tags:["マスコット"], img:"images/snoopy.jpeg", alt:"スヌーピーのあみぐるみ" }
+];
